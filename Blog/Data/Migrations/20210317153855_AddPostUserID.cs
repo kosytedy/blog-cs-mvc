6a3022b2199b6906bf906606a211a014112e42b8
Teddy <kosytedy@yahoo.com>
2021-03-20 17:09:23 +0000

    Added comment model

---
+++ Blog/Data/Migrations/20210317153855_AddPostUserID.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Blog.Data.Migrations
+{
+    public partial class AddPostUserID : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "UserID",
+                table: "Post",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserID",
+                table: "Post");
+        }
+    }
+}
---
+++ Blog/Models/Comment.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Blog.Models
+{
+    public class Comment
+    {
+        public int CommentID { get; set; }
+
+        public string UserID { get; set; }
+
+        public int PostID { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        [DataType(DataType.Text)]
+        public string Content { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedAt { get; set; }
+
+        public Comment()
+        {
+        }
+    }
+}
