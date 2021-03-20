7527f014155cf3ca8687d8272ff6dd3452fa166a
Teddy <kosytedy@yahoo.com>
2021-03-20 18:15:39 +0000

    Created CommentController

---
+++ Blog/Controllers/CommentController.cs
@@ -0,0 +1,167 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Blog.Data;
+using Blog.Models;
+
+namespace Blog.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CommentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Comment
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Comment.Include(c => c.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Comment/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comment = await _context.Comment
+                .Include(c => c.User)
+                .FirstOrDefaultAsync(m => m.CommentID == id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            return View(comment);
+        }
+
+        // GET: Comment/Create
+        public IActionResult Create()
+        {
+            ViewData["UserID"] = new SelectList(_context.Users, "Id", "Id");
+            return View();
+        }
+
+        // POST: Comment/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PostID,Content")] Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                //var post = await _context.Post
+                //.FirstOrDefaultAsync(m => m.PostId == comment.PostID);
+                //if (post == null)
+                //{
+                //return NotFound();
+                //}
+                comment.CreatedAt = DateTime.Now;
+                _context.Add(comment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Details), "PostController", new { id = comment.PostID });
+            }
+            //ViewData["UserID"] = new SelectList(_context.Users, "Id", "Id", comment.UserID);
+            return RedirectToAction(nameof(Details), "PostController", new { id = comment.PostID });
+        }
+
+        // GET: Comment/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comment = await _context.Comment.FindAsync(id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserID"] = new SelectList(_context.Users, "Id", "Id", comment.UserID);
+            return View(comment);
+        }
+
+        // POST: Comment/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CommentID,UserID,PostID,Content,CreatedAt")] Comment comment)
+        {
+            if (id != comment.CommentID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(comment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CommentExists(comment.CommentID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserID"] = new SelectList(_context.Users, "Id", "Id", comment.UserID);
+            return View(comment);
+        }
+
+        // GET: Comment/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var comment = await _context.Comment
+                .Include(c => c.User)
+                .FirstOrDefaultAsync(m => m.CommentID == id);
+            if (comment == null)
+            {
+                return NotFound();
+            }
+
+            return View(comment);
+        }
+
+        // POST: Comment/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var comment = await _context.Comment.FindAsync(id);
+            _context.Comment.Remove(comment);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CommentExists(int id)
+        {
+            return _context.Comment.Any(e => e.CommentID == id);
+        }
+    }
+}
---
+++ Blog/ViewModels/PostPageData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Blog.Models;
+
+namespace Blog.ViewModels
+{
+    public class PostPageData
+    {
+        public Post Post { get; set; }
+
+        public List<Comment> Comments { get; set; }
+
+        public PostPageData()
+        {
+        }
+    }
+}
